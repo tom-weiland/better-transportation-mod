886bd851a2f924e10a39759812a14cb9b576a2f4
Tom Weiland <tomdweiland@gmail.com>
2025-05-12 22:10:17 -0700

    Initial commit

---
+++ src/main/java/net/tomweiland/better_transportation/BetterTransportation.java
@@ -0,0 +1,16 @@
+package net.tomweiland.better_transportation;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterTransportation implements ModInitializer {
+	public static final String MOD_ID = "better_transportation";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/net/tomweiland/better_transportation/BetterTransportationDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tomweiland.better_transportation;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterTransportationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
